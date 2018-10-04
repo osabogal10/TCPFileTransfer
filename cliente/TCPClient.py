59fbc7dad8bd8a65ad77cc85009f7d6b8ca1fd2d
Zulma Castañeda <zl.castaneda10@uniandes.edu.co>
2018-10-04 11:24:29 -0500

    final

---
+++ cliente/TCPClient.py
@@ -0,0 +1,91 @@
+import socket, sys, threading, hashlib,time, logging,os
+from time import gmtime, strftime
+
+from sys import stderr
+from logging import getLogger, StreamHandler, Formatter, DEBUG
+from time import sleep
+
+#configuracion del logger
+l = getLogger()
+os.makedirs(os.path.dirname('./logs/TCP.log'), exist_ok=True)
+logging.basicConfig(format='%(message)s', filename='./logs/TCP.log',  level=logging.DEBUG)
+sh = StreamHandler(stderr)
+sh.setLevel(DEBUG)
+f = Formatter(' %(message)s')
+sh.setFormatter(f)
+l.addHandler(sh)
+l.setLevel(DEBUG)
+
+
+host, port = '157.253.205.7', 9000
+hasher = hashlib.md5()
+SIZE=32000
+
+showtime = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+l.info('%s;%s','DATE',showtime)
+
+
+class recv_data :
+    mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    mysocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    mysocket.connect((host, port))
+    def __init__(self):
+        print('Connected successfully')
+        start_time = time.time()
+        filename = self.mysocket.recv(SIZE)
+        filesize = self.mysocket.recv(SIZE)
+        idCliente = self.mysocket.recv(SIZE)
+        data = self.mysocket.recv(SIZE)
+        i=0
+        bytesReceived=0
+        f = open(filename.decode('utf-8'), 'wb+')
+        while data != bytes(''.encode()):
+            #print(data)
+            f.write(data)
+            data = self.mysocket.recv(SIZE)
+            bytesReceived=bytesReceived+len(data);
+            i=i+1
+            if data == b'Fin' :
+
+                break
+        buf = f.read()
+        hasher.update(buf)
+        hash_cliente = hasher.hexdigest()
+
+        print('hash: ', hasher.hexdigest())
+
+
+
+        l.info('%s;%s', 'FILE_NAME', filename.decode('utf-8'))
+        l.info('%s;%s', 'FILE_SIZE', filesize.decode('utf-8'))
+        l.info('%s;%s', 'CLIENT', idCliente.decode('utf-8'))
+
+        bytesSent = self.mysocket.recv(SIZE)
+        numPack = self.mysocket.recv(SIZE)
+
+        hash_servidor = self.mysocket.recv(SIZE)
+        hash_servidor = hash_servidor.decode('utf-8')
+        if hash_servidor == hash_cliente:
+            l.info('FILE_DELIVERY;SUCCESS')
+
+        else:
+            l.info('FILE_DELIVERY;FAILURE')
+
+        l.info('%s;%s', 'BYTES_SENT', bytesSent.decode('utf-8'))
+        l.info('%s;%s', 'BYTES_RECEIVED', str(bytesReceived-3))
+
+        elapsed_time = time.time() - start_time
+        showtime = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+        print('TIME ELAPSED: ', elapsed_time)
+        l.info('%s;%s', 'PACKETS SENT', numPack.decode('utf-8'))
+        l.info('%s;%s', 'PACKETS RECEIVED', i-1)
+
+        l.info('%s;%s', 'ELAPSED_TIME', elapsed_time)
+        l.info('------------------------------')
+        logging.shutdown()
+        os.rename('./logs/TCP.log', './logs/TCP{}.log'.format(idCliente.decode('utf-8')))
+
+
+
+
+re = recv_data()
---
+++ servidor/TCPServer.py
@@ -0,0 +1,70 @@
+import socket, threading, os, hashlib, sys
+from time import sleep
+
+
+host, port = '', 9000
+SIZE = 32000
+hasher = hashlib.md5()
+
+
+
+
+class transfer :
+
+    mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    mysocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    def __init__(self):
+        num_clients = int(sys.argv[1])
+        file_name = sys.argv[2]
+        self.mysocket.bind((host, port))
+        print(' Server is ready ..')
+        self.mysocket.listen(5)
+        threads = []
+        id_cliente =1
+        while num_clients >0:
+            conn, addr = self.mysocket.accept()
+            size = os.path.getsize(file_name)
+            print(' file size : {}'.format(str(size)))
+            send_thread = threading.Thread(target = self.send_file, args=(file_name, size, conn, addr, id_cliente ))
+            threads.append(send_thread)
+            num_clients = num_clients - 1
+            id_cliente=id_cliente+1
+        for thread in threads:
+            thread.start()
+
+    def send_file(self, file_name, size, conn, addr, id_cliente):
+        size = os.path.getsize(file_name)
+        conn.send(file_name.encode('utf-8'))
+        conn.send(str(size).encode('utf-8'))
+        conn.send(str(id_cliente).encode('utf-8'))
+        i = 0
+        bytesSent = 0
+        print(' file size : {}'.format(str(size)))
+        with open(file_name, 'rb') as file:
+            data = file.read(SIZE)
+            conn.send(data)
+            while data != bytes(''.encode()):
+                #print(data)
+                data = file.read(SIZE)
+                sent = conn.send(data)
+                i = i+1
+                bytesSent = bytesSent+sent
+                if sent != SIZE:
+                    sent = conn.send(b'Fin')
+                    print('Fin')
+                    break
+            conn.send(str(bytesSent).encode('utf-8'))
+            sleep(0.5)
+            conn.send(str(i).encode('utf-8'))
+            buf = file.read()
+            hasher.update(buf)
+            hash_servidor = hasher.hexdigest()
+            conn.send(str(hash_servidor).encode('utf-8'))
+            print(' File sent successfully.')
+
+
+
+
+
+Transfer = transfer()
